0d493a8b0b9e009f82d9f138fe03ee604ddd33d5
icer-CHN <icer.chn@outlook.com>
2015-09-25 16:42:56 +0800

    cnbeta progress stuff

---
+++ cnbeta/src/main/java/com/icer/cnbeta/adapter/LatestListAdapter.java
@@ -0,0 +1,83 @@
+package com.icer.cnbeta.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.icer.cnbeta.R;
+import com.icer.cnbeta.volley.entity.Latest;
+
+import java.util.ArrayList;
+
+/**
+ * Created by icer on 2015-09-25.
+ */
+public class LatestListAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private ArrayList<Latest> mData;
+
+    public LatestListAdapter(Context context, ArrayList<Latest> data) {
+        mContext = context;
+        mData = data;
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_latest, parent, false);
+            holder = new ViewHolder();
+            holder.initView(convertView);
+            convertView.setTag(holder);
+        }
+        if (holder == null)
+            holder = (ViewHolder) convertView.getTag();
+        holder.fillInData(mData.get(position));
+        return convertView;
+    }
+
+    public void refreshData(ArrayList<Latest> list) {
+        //»±…Ÿsid≈–∂œ
+        notifyDataSetChanged();
+    }
+
+    public void addData(ArrayList<Latest> list) {
+        //»±…Ÿsid≈–∂œ
+        mData.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    private class ViewHolder {
+        private TextView title;
+        private TextView summary;
+
+        private void initView(View view) {
+            title = (TextView) view.findViewById(R.id.item_title_tv);
+            summary = (TextView) view.findViewById(R.id.item_summary_tv);
+        }
+
+        private void fillInData(Latest bean) {
+            title.setText(bean.title);
+            summary.setText(bean.summary);
+        }
+    }
+}
--- cnbeta/src/main/java/com/icer/cnbeta/ui/MainActivity.java
+++ cnbeta/src/main/java/com/icer/cnbeta/ui/MainActivity.java
@@ -10,9 +10,13 @@ import com.alibaba.fastjson.JSON;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.icer.cnbeta.R;
+import com.icer.cnbeta.adapter.LatestListAdapter;
 import com.icer.cnbeta.app.BaseActivity;
 import com.icer.cnbeta.manager.RequestManager;
 import com.icer.cnbeta.volley.LatestListBean;
+import com.icer.cnbeta.volley.entity.Latest;
+
+import java.util.ArrayList;
 
 /**
  * Created by icer on 2015-09-24.
@@ -26,12 +30,15 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private ListView mListView;
 
+    private LatestListAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initData();
         initView();
+        initAdapter();
         initToolBar();
         regListener();
         requestLatest();
@@ -55,6 +62,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
     private void initView() {
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
         mListView = (ListView) findViewById(R.id.list);
+    }
+
+    private void initAdapter() {
+        mAdapter = new LatestListAdapter(this, new ArrayList<Latest>());
+        mListView.setAdapter(mAdapter);
     }
 
     private void initToolBar() {
@@ -84,7 +96,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
             public void onResponse(String s) {
                 logI(TAG, s);
                 LatestListBean latestListBean = JSON.parseObject(s, LatestListBean.class);
-                logI(TAG, latestListBean.toString());
+                mAdapter.addData(latestListBean.result);
                 stopRefresh();
             }
         }, new Response.ErrorListener() {
